fcda07ed639fd8abaaee7f709af2c23448444eef
Chien-Ming Liao <linex1008@gmail.com>
2017-06-13 00:18:01 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+    $host = 'localhost';
+	$id = "yuroko13";
+	$pw = "yuroko13";
+	$sheet = "members";
+	$conn = mysqli_connect($host,$id,$pw,$sheet) or die("failed");
+    
+?>
--- login_check.php
+++ login_check.php
@@ -1,3 +1,19 @@
 <?php
-    echo "晚點來寫ㄅ";
+	require_once('connect.php');
+    $account = $_POST['taccount'];
+    $password = $_POST['tpassword'];
+    $sql = "select * from members_info where account = '".$account."'";
+    $result = mysqli_query($conn,$sql);
+    $asso_result = mysqli_fetch_assoc($result);
+    if ($account == $asso_result['account']) {
+        if ($password == $asso_result['password']){
+            echo 'Welcome '.$asso_result['account'].' !';
+        }
+        else {
+        echo 'Your password is not correct.';
+        }
+	}
+    else {
+        echo "This account is not exist.";
+    }
 ?>
